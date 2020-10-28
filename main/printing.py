d7e8f110e405b0c209aaa5a94886ef83e4554922
Jan Ruzicka <jan.ruzicka.1@plus4u.net>
2020-10-28 09:07:41 +0100

    Fix #69

--- main/printing.py
+++ main/printing.py
@@ -43,9 +43,9 @@ class Printing(object):
     def exportPDF(self, extent, path):
         self.exportAll(extent, path, 1.1)
 
-    def exportTiles(self, extent, path):
-        widthmax = 2718.16
-        heightmax = 1772.72
+    def exportTiles(self, extent, path, scale):
+        widthmax = (271.816 * scale) / 0.64
+        heightmax = (177.272 * scale) / 0.64
         widthmap = extent.width()
         heightmap = extent.height()
         cols = int(widthmap / widthmax) + 1 # we need to handle round - may use roundup ot increase number of cols by 1
@@ -59,8 +59,8 @@ class Printing(object):
                                     extent.yMaximum() - (row * heightmax),
                                     extent.xMinimum() + (col * widthmax) + widthmax,
                                     extent.yMaximum() - (row * heightmax) - heightmax)
-                tilemap += '<td style="border: 1px solid; width: 100px; height: 50px; text-align: center"><a href="report_' + str(row) + '_' + str(col) + '.pdf">' + str(row) + '-' + str(col) + '&nbsp;<img src="styles/pdf.png" alt="PDF" width="40"></a></a></td>\n'
-                self.export(rect, path + "report_" + str(row) + "_" + str(col) + ".pdf", 1.0)
+                tilemap += '<td style="border: 1px solid; width: 100px; height: 50px; text-align: center"><a href="' + str(scale) + '_report_' + str(row) + '_' + str(col) + '.pdf">' + str(row) + '-' + str(col) + '&nbsp;<img src="styles/pdf.png" alt="PDF" width="40"></a></a></td>\n'
+                self.export(rect, path + str(scale) + "_report_" + str(row) + "_" + str(col) + ".pdf", 1.0)
             tilemap += '</tr>\n'
         tilemap += '</table>\n'
 
@@ -69,7 +69,7 @@ class Printing(object):
             filedata = file.read()
 
         # Replace the target string
-        filedata = filedata.replace("<!--tilemap-->", tilemap)
+        filedata = filedata.replace("<!--tilemap" + str(scale) + "-->", tilemap)
 
         # Write the file out again
         with open(path + "report.html", 'w') as file:
@@ -98,4 +98,8 @@ class Printing(object):
         layout.updateSettings()
         exporter = QgsLayoutExporter(layout)
         exporter.exportToPdf(path + "report.pdf", QgsLayoutExporter.PdfExportSettings())
-        self.exportTiles(composer_map.extent(), path)
+        #self.exportTiles(composer_map.extent(), path, 50)
+        #composer_map.zoomToExtent(extent)
+        self.exportTiles(composer_map.extent(), path, 25)
+        composer_map.zoomToExtent(extent)
+        self.exportTiles(composer_map.extent(), path, 10)
